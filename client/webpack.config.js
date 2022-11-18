ff684ecd713ce7100727935e8b9be0848598ea63
Crestatic <dtnguy86@gmail.com>
2022-11-17 17:19:07 -0800

    ran instal for all packages.  Built dist folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,9 +30,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Cant have too many text editors!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
