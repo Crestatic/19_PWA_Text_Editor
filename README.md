5a0d43a67b7de8e4dcc6bfd0ecc59d42f85d9d6b
Crestatic <dtnguy86@gmail.com>
2022-11-17 22:53:24 -0800

    added fix to database.js for getDB.  Readme completed.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # 19_PWA_Text_Editor
+
+## Table of Content
+* [Description](#description)
+* [Language/Technologies](#language-and-technologies)
+* [Installation](#installation)
+* [Links](#Links)
+
+## Description
+This Progressive Web Application is a text editor that enables the user to install the app to a local device.
+
+## Language and Technologies
+* HTML/CSS/Javascript
+* Webpack
+* Express
+* Babel
+* IndexedDB
+
+## Installation
+Please run in your ternimal after cloning:
+* npm run install
+* npm run build
+* npm run start:dev
+
+## Links
+* Github: https://github.com/Crestatic/19_PWA_Text_Editor
+* Heroku: https://lit-wave-84690.herokuapp.com/
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -30,7 +30,7 @@ export const putDb = async (content) => {
 
   // Get confirmation of the request
   const result = await request;
-  console.log('data saved to database', result);
+  console.log('data saved to database', result.valueOf);
 }
   
 
@@ -50,12 +50,12 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 }
 
 // initialize the database
